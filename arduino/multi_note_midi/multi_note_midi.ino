7ca3ee235bcaf24108408966159b74c024355ad3
aleathwick <leathwick@gmail.com>
2021-12-13 22:52:26 +1300

    refactor adc params into multidimensional arrays

--- arduino/multi_note_midi/multi_note_midi.ino
+++ arduino/multi_note_midi/multi_note_midi.ino
@@ -7,8 +7,7 @@
 #include <Adafruit_MCP3008.h>
 
 // ADCs
-Adafruit_MCP3008 adc1;
-Adafruit_MCP3008 adc2;
+Adafruit_MCP3008 adcs[2];
 
 // USB MIDI object
 Adafruit_USBD_MIDI usb_midi;
@@ -17,36 +16,39 @@ Adafruit_USBD_MIDI usb_midi;
 // and attach usb_midi as the transport.
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
 
-int adc1Count = 2;
-int adc2Count = 2;
-int adc1Bits = 1024;
-int adc2Bits = 1024;
-int adc1Pins[] = {0,1};
-int adc2Pins[] = {0,1};
-int adc1Notes[] = {64,65};
-int adc2Notes[] = {66,67};
+int adcCount = 2; // n adcs
+int adcBits = 1024;
+int adcSelects[] = {17, 15}; // pins for selecting adcs
+int adcSensorCounts[] = {2,2}; // sensors per adc
+// last dimension must be of size max(adcSensorCounts)
+int adcPins[][2] = {{0,1},
+                  {0,1}};
+int adcNotes[][2] = {{64,65},
+                  {66,67}};
 
 void setup()
 {
-
+  Serial.begin(115200);
   // begin ADC
   // can set SPI default pins
   //  SPI.setCS(5);
   //  SPI.setSCK(2);
   //  SPI.setTX(3);
   //  SPI.setRX(4);
-  adc1.begin(17);
-  adc2.begin(15);
+  for (int i = 0; i < adcCount; i++)
+  {
+    adcs[i].begin(adcSelects[i]);
+  }
 
   pinMode(LED_BUILTIN, OUTPUT);
   
-  //usb_midi.setStringDescriptor("TinyUSB MIDI");
+  usb_midi.setStringDescriptor("Laser Piano");
 
   // Initialize MIDI, and listen to all MIDI channels
   // This will also call usb_midi's begin()
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
-  Serial.begin(115200);
+  
 
   // wait until device mounted
   while( !USBDevice.mounted() ) delay(1);
@@ -57,17 +59,17 @@ void loop()
   static uint32_t start_ms = 0;
   if ( millis() - start_ms > 266 )
   {
-    for (int i = 0; i < adc1Count; i++)
-    {
-      // send with velocity 0 for note off
-      MIDI.sendNoteOn(adc1Notes[i], adc1.readADC(adc1Pins[i]) * 127 / adc1Bits, 1);
-      Serial.print(adc1.readADC(adc1Pins[i]) * 127 / adc1Bits);
-      Serial.print(" ");
-      Serial.print(adc1.readADC(adc1Pins[i]));
-      Serial.print(" ");
+    for (int i = 0; i < adcCount; i++)
+    {    
+      for (int j = 0; j < adcSensorCounts[i]; j++)
+      {
+        // use sendNoteOff for note offs
+        MIDI.sendNoteOn(adcNotes[i][j], adcs[i].readADC(adcPins[i][j]) * 127 / adcBits, 1);
+        Serial.print(adcs[i].readADC(adcPins[i][j]));
+        Serial.print(" ");
+      }
     }
     Serial.print("\n");
-    
     // Serial.printf("%d, %d \n", adc1.readADC(0), adc1.readADC(1));
     
     start_ms += 266;

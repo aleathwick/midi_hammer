c30d6a5d81b3a39dca0a90a34c8ed36ea4aa36a7
Andrew Leathwick <andrew.leathwick@twgroup.co.nz>
2022-12-07 22:43:59 +1300

    move keyhammer class into .h and .cpp files
    Need to fix up things this breaks.

--- arduino/KeyHammer/KeyHammer.cpp
+++ arduino/KeyHammer/KeyHammer.cpp
@@ -1,77 +1,31 @@
 // https://cplusplus.com/doc/tutorial/classes/
 // https://paulmurraycbr.github.io/ArduinoTheOOWay.html
 // https://www.circuitbasics.com/programming-with-classes-and-objects-on-the-arduino/
+#include "KeyHammer.h"
+#include <math.h>
 // #include <Adafruit_MCP3008.h>
 // #include <elapsedMillis.h>
 
-class KeyHammer
-{
-    // for how to make this work with mutliple types (MCP3008 or MCP3208), see here:
-    // https://stackoverflow.com/questions/69441566/how-to-declare-a-class-member-that-may-be-one-of-two-classes
-    Adafruit_MCP3008 adc;
-    // define the range of the sensors, with sensorFullyOn being the key fully depressed
-  // this will work regardless of sensorFullyOn < sensorFullyOff or sensorFullyOff < sensorFullyOn
-    int sensorFullyOn;
-    int sensorFullyOff;
-    // threshold for hammer to activate note
-    int noteOnThreshold;
-    // threshold for key to trigger noteoff
-    int noteOffThreshold;
-    int sensorMin;
-    int sensorMax;
 
-    int pin;
-    int pitch;
-    
-    int keyPosition;
-    int lastKeyPosition;
-    // key and hammer speeds are measured in adc bits per microsecond
-    double keySpeed;
+// use a constructor initializer list for adc, otherwise the reference won't work
+KeyHammer::KeyHammer (int(*adcFnPtr)(void), int pin, int pitch, char operationMode='h', int sensorFullyOn=430, int sensorFullyOff=50)
+  : adcFnPtr(adcFnPtr), pin(pin), pitch(pitch), operationMode(operationMode), sensorFullyOn(sensorFullyOn), sensorFullyOff(sensorFullyOff) {
 
-    double hammerPosition;
-    double hammerSpeed;
-    double hammer_travel;
-    double gravity;
-
-    bool noteOn;
-    double velocity;
-    int velocityIndex;
-
-    char printMode;
-
-
-    void update_key();
-    void update_hammer();
-    void check_note_on();
-    void check_note_off();
-
-  public:
-    KeyHammer(Adafruit_MCP3008 adc, int pin, int pitch, int sensorFullyOn, int sensorFullyOff);
-    void step();
-
-    elapsedMicros elapsed;
-};
-
-KeyHammer::KeyHammer (Adafruit_MCP3008 adc, int pin, int pitch, int sensorFullyOn=430, int sensorFullyOff=50) {
-  adc = adc;
-  pin = pin;
-  pitch = pitch;
-  sensorFullyOn = sensorFullyOn;
-  sensorFullyOff = sensorFullyOff;
   sensorMax = max(sensorFullyOn, sensorFullyOff);
   sensorMin = min(sensorFullyOn, sensorFullyOff);
 
-  noteOnThreshold = sensorFullyOn + 1.2 * (sensorFullyOn - sensorFullyOff);
+  noteOnThreshold = sensorFullyOn + 0.06 * (sensorFullyOn - sensorFullyOff);
   noteOffThreshold = sensorFullyOn - 0.5 * (sensorFullyOn - sensorFullyOff);
 
   // gravity for hammer, measured in adc bits per microsecond per microsecond
   // if the key press is ADC_range, where ADC_range is abs(sensorFullyOn - sensorFullyOff)
   // hammer travel in mm; used to calculate gravity in adc bits
-  hammer_travel = 4.5;
+  hammer_travel = 0.2;
   gravity = (sensorFullyOn - sensorFullyOff) / (hammer_travel * (double)9810000000);
 
   keyPosition = sensorFullyOff;
   lastKeyPosition = sensorFullyOff;
+  rawADC = sensorFullyOff;
   keySpeed = 0.0;
   hammerPosition = sensorFullyOff;
   hammerSpeed = 0.0;
@@ -80,16 +34,23 @@ KeyHammer::KeyHammer (Adafruit_MCP3008 adc, int pin, int pitch, int sensorFullyO
 
   elapsed = 0;
 
+  lastControlValue = 0;
+  controlValue = 0;
+
   printMode='p';
 }
 
 void KeyHammer::update_key () {
   lastKeyPosition = keyPosition;
-  keyPosition = adc.readADC(pin);
+  rawADC = getAdcValue();
+  keyPosition = rawADC;
   // constrain key position to be within the range determined by sensor max and min
   keyPosition = min(keyPosition, sensorMax);
   keyPosition = max(keyPosition, sensorMin);
 
+}
+
+void KeyHammer::update_keyspeed () {
   keySpeed = (keyPosition - lastKeyPosition) / (double)elapsed;
 }
 
@@ -116,7 +77,7 @@ void KeyHammer::check_note_on () {
     velocity = hammerSpeed;
     velocityIndex = round(hammerSpeed * hammerSpeedScaler);
     velocityIndex = min(velocityIndex, velocityMapLength);
-    MIDI.sendNoteOn(pitch, velocityMap[velocityIndex], 1);
+    MIDI.sendNoteOn(pitch, velocityMap[velocityIndex], 2);
     noteOn = true;
     if (printMode == 'i'){ //&& ((i == 0 && j == 0) || (i == 1 && j == 2))){
       Serial.printf("\n note on: hammerSpeed %f, velocityIndex %d, velocity %d pitch %d \n", velocity, velocityIndex, velocityMap[velocityIndex], pitch);
@@ -129,7 +90,7 @@ void KeyHammer::check_note_on () {
 void KeyHammer::check_note_off () {
   if (noteOn){
     if ((keyPosition > noteOffThreshold) == (sensorFullyOff > sensorFullyOn)) {
-      MIDI.sendNoteOff(pitch, 64, 1);
+      MIDI.sendNoteOff(pitch, 64, 2);
       if (printMode == 'i'){
         Serial.printf("note off: noteOffThreshold %d, adcValue %d, velocity %d  pitch %d \n", noteOffThreshold, keyPosition, 64, pitch);
       }
@@ -138,20 +99,76 @@ void KeyHammer::check_note_off () {
   }
 }
 
-void KeyHammer::step () {
+void KeyHammer::step_hammer () {
   update_key();
+  update_keyspeed();
   update_hammer();
+  // test();
   check_note_on();
   check_note_off();
   // print some stuff
   if (printMode == 'p'){
     // Serial.printf("%d %f %f ", keyPosition, hammerPosition, hammerSpeed);
     Serial.printf("hammer_%d:%f,", pitch, hammerPosition);
-    Serial.printf("keySpeed_%d:%f,", pitch, keySpeed * 10000);
-    Serial.printf("hammerSpeed_%d:%f,", pitch, hammerSpeed * 10000);
+    // Serial.printf("keySpeed_%d:%f,", pitch, keySpeed * 10000);
+    // Serial.printf("hammerSpeed_%d:%f,", pitch, hammerSpeed * 10000);
+    Serial.printf("rawADC_%d:%d,", pitch, rawADC);
+    Serial.printf("elapsed_%d:%d,", pitch, (int)elapsed);
+    // Serial.printf("pin_%d:%d,", pitch, pin);
     // this newline may need to go after all keys have printed? I'm unsure how the serial plotter works.
-    Serial.print('\n');
+    // Serial.print('\n');
   }
   elapsed = 0;
 }
 
+
+void KeyHammer::step_pedal () {
+  update_key();
+  lastControlValue = controlValue;
+  // this could be sped up by precomputing the possible values
+  controlValue = (int)((keyPosition - sensorFullyOff) / double(sensorFullyOn - sensorFullyOff) * 127);
+  if (controlValue != lastControlValue) {
+    // control numbers:
+    // 1 = Modulation wheel
+    // 2 = Breath Control
+    // 7 = Volume
+    // 10 = Pan
+    // 11 = Expression
+    // 64 = Sustain Pedal (on/off)
+    // 65 = Portamento (on/off)
+    // 71 = Resonance (filter)
+    // 74 = Frequency Cutoff (filter)
+    MIDI.sendControlChange(	64, controlValue, 2);
+  }
+  // print some stuff
+  if (printMode == 'p'){
+    Serial.printf("key_%d:%f,", pitch, keyPosition);
+    Serial.printf("rawADC_%d:%d,", pitch, rawADC);
+    Serial.printf("elapsed_%d:%d,", pitch, (int)elapsed);
+    Serial.printf("controlValue_%d:%d,", controlValue, (int)elapsed);
+  }
+  elapsed = 0;
+}
+
+void KeyHammer::step () {
+  if (operationMode=='h')
+  {
+    step_hammer();
+  } else if (operationMode=='p')
+  {
+    step_pedal();
+  }
+}
+
+void KeyHammer::test () {
+  Serial.printf("value:%d\n", getAdcValue());
+}
+
+int KeyHammer::getAdcValue () {
+  return adcFnPtr();
+}
+
+for (int i = 0; i < 5; i++)
+{
+  
+}
--- arduino/KeyHammer/KeyHammer.h
+++ arduino/KeyHammer/KeyHammer.h
@@ -1,12 +1,68 @@
 // https://cplusplus.com/doc/tutorial/classes/
 // https://paulmurraycbr.github.io/ArduinoTheOOWay.html
 // https://www.circuitbasics.com/programming-with-classes-and-objects-on-the-arduino/
+
+#pragma once
+#include <elapsedMillis.h>
+#include "MIDI.h"
+
 class KeyHammer
 {
+    // a pointer to a function that will return the position of the key
+    // see here: https://forum.arduino.cc/t/function-as-a-parameter-in-class-object-function-pointer-in-library/461967/7
+    int(*adcFnPtr)(void);
+    // define the range of the sensors, with sensorFullyOn being the key fully depressed
+  // this will work regardless of sensorFullyOn < sensorFullyOff or sensorFullyOff < sensorFullyOn
+    int sensorFullyOn;
+    int sensorFullyOff;
+    // threshold for hammer to activate note
+    int noteOnThreshold;
+    // threshold for key to trigger noteoff
+    int noteOffThreshold;
+    int sensorMin;
+    int sensorMax;
+
+    int pin;
+    int pitch;
+    
+    int rawADC;
+    int keyPosition;
+    int lastKeyPosition;
+    // key and hammer speeds are measured in adc bits per microsecond
+    double keySpeed;
+
+    double hammerPosition;
+    double hammerSpeed;
+    double hammer_travel;
+    double gravity;
+
+    bool noteOn;
+    double velocity;
+    int velocityIndex;
+
+    // parameters for pedal mode
+    int lastControlValue;
+    int controlValue;
+
+    char printMode;
+
+
+    void update_key();
+    void update_keyspeed();
+    void update_hammer();
+    void check_note_on();
+    void check_note_off();
+    void test();
+    void step_hammer();
+    void step_key();
+    void step_pedal();
+
   public:
-    KeyHammer(int pin);
-    void dot();
-    void dash();
-  private:
-    int _pin;
+    KeyHammer(int(*adcFnPtr)(void), int pin, int pitch, char operationMode, int sensorFullyOn, int sensorFullyOff);
+    void step();
+    // operation mode switches between operation as a hammer simulation key, a key, or a pedal
+    char operationMode;
+    int getAdcValue(void);
+
+    elapsedMicros elapsed;
 };

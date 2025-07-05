1edbda71098b85acd76f7c3d4202e564e655ac99
aleathwick <leathwick@gmail.com>
2025-07-06 06:00:00 +1200

    reduce buffer size (was too much for teensy with 84 keys)

--- arduino/src/config.h
+++ arduino/src/config.h
@@ -5,4 +5,9 @@
 // #define TEENSY
 #define PICO
 
-#define BUFFER_SIZE 30
+// number of values to store in buffers
+// e.g. for the ADC buffer
+// with 8 keys, 3 int buffers, 1 float buffer, this fails above 3000 (not enough ram1)
+// we need at least enough for the savitzy-golay filters - longest SG filter is 99
+// if dumping note-on adc data, then longer is better but need to baance number of keys with length of buffers
+#define BUFFER_SIZE 100

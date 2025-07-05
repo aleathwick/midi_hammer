f900ee352e0e9bcaa24e3c89acc27bb4f6a57815
aleathwick <leathwick@gmail.com>
2025-07-06 06:00:00 +1200

    add missing src files (forgot to commit earlier)

---
+++ arduino/src/MidiSenderDummy.cpp
@@ -0,0 +1,16 @@
+#include "MidiSenderDummy.h"
+
+void MidiSenderDummy::sendNoteOn(int pitch, int velocity, int channel) {
+}
+
+void MidiSenderDummy::sendNoteOff(int pitch, int velocity, int channel) {
+}
+
+void MidiSenderDummy::sendControlChange(int controlNumber, int controlValue, int channel) {
+}
+
+void MidiSenderDummy::initialize() {
+}
+
+void MidiSenderDummy::loopEnd() {
+}
